be31de3b5979332adc0ab336b68a160ea43fbd8a
zakaZak! <evg.zadorognyh41@gmail.com>
2020-10-27 13:45:21 +0300

    edit link to logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <header className='header'>
             <NavLink to='/profile' className="header__link">
-                <img src='../../images/logo.png' className='header__logo' alt='logo app' />
+                <img src='../../images/hash-logo.png' className='header__logo' alt='logo app' />
             </NavLink>
         </header>
     )
