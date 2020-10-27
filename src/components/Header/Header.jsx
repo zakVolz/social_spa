e2e0f536f6aa86195652c8b606eef4cf447272ca
zakaZak! <evg.zadorognyh41@gmail.com>
2020-10-27 14:17:36 +0300

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
+import logo from '../../images/logo.png';
 
 const Header = () => {
-    return (
-        <header className='header'>
-            <NavLink to='/profile' className="header__link">
-                <img src='../../images/hash-logo.png' className='header__logo' alt='logo app' />
-            </NavLink>
-        </header>
-    )
+  return (
+    <header className='header'>
+      <NavLink to='/profile' className="header__link">
+        <img src={logo} className='header__logo' alt='logo app' />
+      </NavLink>
+    </header>
+  )
 }
 
 export default Header;
